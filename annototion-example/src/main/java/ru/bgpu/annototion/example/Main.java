e89b4c54c9bae43bfa509622b5ae113e4db6ed87
informatic <piligrim.a3@gmail.com>
2021-02-02 09:21:31 +0900

    add: logback

--- annototion-example/src/main/java/ru/bgpu/annototion/example/Main.java
+++ annototion-example/src/main/java/ru/bgpu/annototion/example/Main.java
@@ -3,9 +3,16 @@
 package ru.bgpu.annototion.example;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class Main {
     
+    private static final Logger logger = LoggerFactory.getLogger(
+            Main.class);
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
     }
 }
