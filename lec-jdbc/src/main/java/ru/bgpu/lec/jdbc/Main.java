b0d1adcee5be594217a7a2ea27483413d28d72e0
informatic <piligrim.a3@gmail.com>
2021-02-09 10:31:00 +0900

    jdbc h2 example

--- lec-jdbc/src/main/java/ru/bgpu/lec/jdbc/Main.java
+++ lec-jdbc/src/main/java/ru/bgpu/lec/jdbc/Main.java
@@ -1,6 +1,19 @@
 package ru.bgpu.lec.jdbc;
 
+import java.sql.*;
+
 public class Main {
 
-    
+    public static void main(String[] args) {
+        String url = "jdbc:h2:mem:";
+        try (Connection con = DriverManager.getConnection(url);
+             Statement stm = con.createStatement();
+             ResultSet rs = stm.executeQuery("SELECT 1+1")) {
+            if (rs.next()) {
+                System.out.println(rs.getInt(1));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
