bf25e648be17a6c96bd9af813bddff08cd5a5c51
informatic <piligrim.a3@gmail.com>
2021-02-09 11:11:25 +0900

    edit: init default and select query

--- lec-jdbc/src/main/java/ru/bgpu/lec/jdbc/Main.java
+++ lec-jdbc/src/main/java/ru/bgpu/lec/jdbc/Main.java
@@ -5,13 +5,25 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
-        String url = "jdbc:h2:mem:";
-        try (Connection con = DriverManager.getConnection(url);
-             Statement stm = con.createStatement();
-             ResultSet rs = stm.executeQuery("SELECT 1+1")) {
-            if (rs.next()) {
-                System.out.println(rs.getInt(1));
+        String url = "jdbc:h2:file:./db";
+
+        Base base = new Base(url);
+        base.initDefault(20);
+
+        try (Connection con = base.getConnection()) {
+            Statement statement = con.createStatement();
+            ResultSet set = statement.executeQuery(
+              "SELECT * FROM customers order by name"
+            );
+
+            while(set.next()) {
+                System.out.println(
+                        set.getInt("id")+" "+
+                        set.getString("name")+" "+
+                        set.getInt("age")
+                );
             }
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
