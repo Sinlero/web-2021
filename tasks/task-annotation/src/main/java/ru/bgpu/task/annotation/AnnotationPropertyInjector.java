0d798147133668debc70a1e9f35348322885176f
Sinlero <wanted1345@gmail.com>
2021-02-22 15:06:58 +0900

    add load properties from config

--- tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationPropertyInjector.java
+++ tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationPropertyInjector.java
@@ -1,9 +1,31 @@
 package ru.bgpu.task.annotation;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class AnnotationPropertyInjector {
+
+    private static final String PROPERTY_PATH = "src/main/resources/config.properties";
 
     public static void inject() {
         
+    }
+
+    private static Properties load(String path) {
+        File config = new File(path);
+        Properties props = new Properties();
+        try (FileReader reader = new FileReader(config)) {
+            props.load(reader);
+        } catch (FileNotFoundException exception) {
+            System.out.println("Properties not found");
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return props;
     }
 
 }
