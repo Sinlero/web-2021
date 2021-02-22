e393c2876042ff23819b239016af29f6089c4cbe
Sinlero <wanted1345@gmail.com>
2021-02-22 14:46:54 +0900

    add scanner for fields

--- tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationScanner.java
+++ tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationScanner.java
@@ -1,7 +1,13 @@
 package ru.bgpu.task.annotation;
 
 import org.reflections.Reflections;
+import org.reflections.scanners.FieldAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import ru.bgpu.task.annotation.annotations.ExampleAnnotation;
+import ru.bgpu.task.annotation.annotations.InjectProperty;
+
+import java.lang.reflect.Field;
 
 public class AnnotationScanner {
 
@@ -11,6 +17,14 @@ public class AnnotationScanner {
         for (Class<?> cl : ref.getTypesAnnotatedWith(ExampleAnnotation.class)) {
             ExampleAnnotation findable = cl.getAnnotation(ExampleAnnotation.class);
             System.out.printf("Найден класс: %s, с именем в аннотации: %s%n",cl.getSimpleName(), findable.name());
+        }
+
+        Reflections fieldRef = new Reflections(new ConfigurationBuilder()
+                .setUrls(ClasspathHelper.forPackage("ru.bgpu.task.annotation"))
+                .setScanners(new FieldAnnotationsScanner()));
+        for (Field field : fieldRef.getFieldsAnnotatedWith(InjectProperty.class)) {
+            InjectProperty findable = field.getAnnotation(InjectProperty.class);
+            System.out.printf("Найдено поле: %s, с именем в аннотации: %s%n",field.getName(), findable.name());
         }
     }
 }
