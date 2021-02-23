c3232f02ccd69253fd8d97b2f98eff9fc34dccaa
Sinlero <wanted1345@gmail.com>
2021-02-23 20:42:21 +0900

    add inject shitcode

--- tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationPropertyInjector.java
+++ tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationPropertyInjector.java
@@ -1,21 +1,51 @@
 package ru.bgpu.task.annotation;
 
+import ru.bgpu.task.annotation.annotations.InjectProperty;
+import ru.bgpu.task.annotation.example.ClassWithAnnotatedFields;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Properties;
-import java.util.TreeSet;
 
 public class AnnotationPropertyInjector {
 
     private static final String PROPERTY_PATH = "tasks/task-annotation/src/main/resources/config.properties";
 
-    public static void inject() {
+    public static void inject() throws Exception {
         Properties props = load(PROPERTY_PATH);
-        TreeSet<String> propertyNames = new TreeSet<>(props.stringPropertyNames());
-        for (String propertyName : propertyNames) {
-            System.out.printf("%s = %s\n", propertyName, props.getProperty(propertyName));
+        Class<?> clazz = ClassWithAnnotatedFields.class;
+        Object object = clazz.newInstance();
+        for (Field field : clazz.getFields()) {
+            if (field.isAnnotationPresent(InjectProperty.class)) {
+                String parameter = field.getAnnotation(InjectProperty.class).name();
+                if (!parameter.isEmpty()) {
+                    if (field.getType().equals(Integer.class)) {
+                        field.set(object, Integer.parseInt(props.getProperty(parameter)));
+                    }
+                    if (field.getType().equals(Double.class)) {
+                        field.set(object, Double.parseDouble(props.getProperty(parameter)));
+                    }
+                    if (field.getType().equals(String.class)) {
+                        field.set(object, props.getProperty(parameter));
+                    }
+                } else {
+                    if (field.getType().equals(Integer.class)) {
+                        field.set(object, Integer.parseInt(props.getProperty(field.getName())));
+                    }
+                    if (field.getType().equals(Double.class)) {
+                        field.set(object, Double.parseDouble(props.getProperty(field.getName())));
+                    }
+                    if (field.getType().equals(String.class)) {
+                        field.set(object, props.getProperty(field.getName()));
+                    }
+                }
+            }
+        }
+        for (Field field : object.getClass().getFields()) {
+            System.out.println(field.get(object));
         }
     }
 
