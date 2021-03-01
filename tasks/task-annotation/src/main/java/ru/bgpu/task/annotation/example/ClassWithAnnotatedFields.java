a98e1d0485f5589e638a8e0d4adb7a95e33a499d
Sinlero <wanted1345@gmail.com>
2021-03-01 23:12:00 +0900

    add fields with collections

--- tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/example/ClassWithAnnotatedFields.java
+++ tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/example/ClassWithAnnotatedFields.java
@@ -2,6 +2,10 @@ package ru.bgpu.task.annotation.example;
 
 import ru.bgpu.task.annotation.annotations.InjectProperty;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 public class ClassWithAnnotatedFields {
 
     @InjectProperty(name = "loadName")
@@ -15,4 +19,13 @@ public class ClassWithAnnotatedFields {
 
     @InjectProperty
     public static Double aDouble;
+
+    @InjectProperty(name = "stringsAnnotation")
+    public static ArrayList<String> strings;
+
+    @InjectProperty
+    public static LinkedList<Double> doubles;
+
+    @InjectProperty(name = "integersAnnotation")
+    public static PriorityQueue<Integer> integers;
 }
