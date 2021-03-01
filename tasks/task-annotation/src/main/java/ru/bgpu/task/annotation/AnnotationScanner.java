476e304b71ba9ef518e62e2f7c7851aeec33de94
Sinlero <wanted1345@gmail.com>
2021-03-01 23:12:18 +0900

    remove field scanner

--- tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationScanner.java
+++ tasks/task-annotation/src/main/java/ru/bgpu/task/annotation/AnnotationScanner.java
@@ -18,13 +18,5 @@ public class AnnotationScanner {
             ExampleAnnotation findable = cl.getAnnotation(ExampleAnnotation.class);
             System.out.printf("Найден класс: %s, с именем в аннотации: %s%n",cl.getSimpleName(), findable.name());
         }
-
-        Reflections fieldRef = new Reflections(new ConfigurationBuilder()
-                .setUrls(ClasspathHelper.forPackage("ru.bgpu.task.annotation"))
-                .setScanners(new FieldAnnotationsScanner()));
-        for (Field field : fieldRef.getFieldsAnnotatedWith(InjectProperty.class)) {
-            InjectProperty findable = field.getAnnotation(InjectProperty.class);
-            System.out.printf("Найдено поле: %s, с именем в аннотации: %s%n",field.getName(), findable.name());
-        }
     }
 }
