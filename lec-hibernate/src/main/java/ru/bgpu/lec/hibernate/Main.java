35b8e0159e64b514bcd61070195bc4b99ab05301
I <piligrim.a3@gmail.com>
2021-03-02 12:47:44 +0900

    add: project hibernate

---
+++ lec-hibernate/src/main/java/ru/bgpu/lec/hibernate/Main.java
@@ -0,0 +1,28 @@
+package ru.bgpu.lec.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.persistence.EntityManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        // Create MetadataSources
+        MetadataSources sources = new MetadataSources(registry);
+        // Create Metadata
+        Metadata metadata = sources.getMetadataBuilder().build();
+        // Create SessionFactory
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        EntityManager manager = sessionFactory.createEntityManager();
+
+
+        
+
+    }
+}
