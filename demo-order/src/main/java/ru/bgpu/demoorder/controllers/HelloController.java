669f5742324d21dafcf68caeca435841410c3e99
informatic <piligrim.a3@gmail.com>
2021-03-16 12:27:32 +0900

    add: thymeleaf

--- demo-order/src/main/java/ru/bgpu/demoorder/controllers/HelloController.java
+++ demo-order/src/main/java/ru/bgpu/demoorder/controllers/HelloController.java
@@ -1,16 +1,21 @@
 package ru.bgpu.demoorder.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
 
     @RequestMapping(path = "/{name}",method = RequestMethod.GET)
-    public String hello(@PathVariable(name = "name") String name) {
-        return "Привет "+name+"!";
+    public String hello(
+            @PathVariable(name = "name") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "hello";
     }
 }
